26c04e00745d52b387fec56c84ff1ff98359ad48
JJ.Costa Resende <1000010082@ucan.edu>
2019-06-17 10:58:02 +0100

    App logs funcional

---
+++ src/java/javalogs2/LogsBean.java
@@ -0,0 +1,54 @@
+package javalogs2;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ *
+ * @author jotajota
+ */
+@Named(value = "logsBean")
+@SessionScoped
+public class LogsBean implements Serializable
+{
+
+//    public static Logger logger = LogManager.getFormatterLogger("Teste");
+    public static Logger logger = LogManager.getLogger("Teste");
+    private Pessoa pessoa = new Pessoa();
+    private List<Pessoa> pessoas;
+
+    public LogsBean()
+    {
+        pessoas = new ArrayList<>();
+    }
+
+    public void adicionarPessoas()
+    {
+        logger.info("Hello, servlet!");
+        logger.debug("Log 1", getPessoa().getApelido());
+        logger.fatal("Logs2 %s", getPessoa().getApelido());
+        pessoas.add(getPessoa());
+        pessoa = new Pessoa();
+    }
+
+    public List<Pessoa> getPessoas()
+    {
+        return pessoas;
+    }
+
+    public Pessoa getPessoa()
+    {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa)
+    {
+        this.pessoa = pessoa;
+    }
+
+}
---
+++ src/java/javalogs2/Pessoa.java
@@ -0,0 +1,44 @@
+package javalogs2;
+
+/**
+ *
+ * @author jotajota
+ */
+public class Pessoa
+{
+
+    private String nome;
+    private String apelido;
+    private String idade;
+
+    public String getNome()
+    {
+        return nome;
+    }
+
+    public void setNome(String nome)
+    {
+        this.nome = nome;
+    }
+
+    public String getApelido()
+    {
+        return apelido;
+    }
+
+    public void setApelido(String apelido)
+    {
+        this.apelido = apelido;
+    }
+
+    public String getIdade()
+    {
+        return idade;
+    }
+
+    public void setIdade(String idade)
+    {
+        this.idade = idade;
+    }
+
+}
